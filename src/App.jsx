d4150bac9ef4627ed9170391f6218ac5778f2c90
funkiirabu <phil.vadala@optus.com.au>
2023-06-28 13:12:37 +1000

    Importing React Three Fiber and setting up basic canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { Canvas } from '@react-three/fiber'
 
 export default function App() {
-  return "hello"
+  return <Canvas>
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial color="orange" />
+    </mesh>  
+  </Canvas>
 }
